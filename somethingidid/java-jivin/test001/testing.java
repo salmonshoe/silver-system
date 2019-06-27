82bde194aa92bcaf13d2390d77372866a87ae9a2
salmonshoe <rafaelreynoso273@gmail.com>
2019-06-27 13:50:31 -0400

    practicing a new language

---
+++ somethingidid/java-jivin/test001/testing.java
@@ -0,0 +1,16 @@
+package test001; //Match the package name to the directory your java file is under
+import java.util.Scanner; // Works similarly to Node.js' CLI Inquirer
+
+public class testing {
+    // Focus my attention to 'main' since it will take place
+    // as the entry point to the application. Everything else will get nested inside this class.
+    public static void main(String[] args) {
+        String name = 'Rafael';
+        int age = 23;
+        double bankAccount = 174.88;
+        boolean broke = true;
+        boolean finStable = !true;
+
+        //System.out works similar to console.
+    }
+}
