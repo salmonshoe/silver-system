f18f3223806ebd7efec85cf2ad0ddf36891c70d6
salmonshoe <rafaelreynoso273@gmail.com>
2019-06-28 05:59:02 -0400

    add new data types to work with

--- somethingidid/java-jivin/test001/testing.java
+++ somethingidid/java-jivin/test001/testing.java
@@ -11,6 +11,34 @@ public class testing {
         boolean broke = true;
         boolean finStable = !true;
 
-        //System.out works similar to console.
+        //System.out works similar to console.log
+        //String.format is like JS ES6 syntax of template literals but you use %s
+        //println and print are methods used to log out messages
+        Sytem.out.println(String.format('%s is %s years old b/c %s has %s in his bank account.', name, age, name, bankAccount));
+        System.out.print("This string won't have a newline appended.");
+
+        //Prompt users for input on the command line
+        //First, create a new Scanner that read from the input stream
+        Scanner input = new Scanner (System.in);
+
+        //Collecting lots of text will require nextLine, meanwhile word inputs only use next
+        System.out.println('Enter a mantra that you like to use.');
+        String sentence = input.nextLine();
+
+        System.out.println('What is your favorite word?');
+        String favWord = input.next();
+
+        //Collecting inetegers and doubles have their own input methods
+        System.out.println('On a scale from 1-5, rate me');
+        Int rate = input.nextint();
+
+        System.out.println('Do you remember your GPA?');
+        Double gpa = input.nextDouble();
     }
 }
+
+//First you must compiple. Try to run this command in the directory above where all your Java files are in
+    //javac directoryName/*.java
+
+// Second you run the command to test your code
+    //java directoryName.fileName
